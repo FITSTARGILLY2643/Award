c7cbb4a8aa7558eb7d77f750e7d1682557710fa4
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-17 11:07:27 +0300

    Create the post project view function that renders the page used to upload the project

--- award/views.py
+++ award/views.py
@@ -82,4 +82,21 @@ def profile(request):
     }
     return render(request, 'profile/profile.html', context)
 
+@login_required(login_url='/accounts/login/')
+def postproject(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.author = current_user
+            project.save()
+        return redirect('/')
+    else:
+        form = ProjectForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'post_project.html', context)
+
 
