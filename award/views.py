07d1a2a44f27bc39776138223cbe7e56f8f85843
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-17 11:00:01 +0300

    Create the home view function

--- award/views.py
+++ award/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import *
+from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Projects.objects.all()
+    context = {
+    "projects":projects,
+    }
+    return render(request, 'index.html', locals())
+
